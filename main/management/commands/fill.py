30419bdd38ffd84cea02a36b3e418a281e89ccca
Daria Beliaeva <dariabeliaeva22@gmail.com>
2024-03-01 12:44:44 -0800

    changes made

--- main/management/commands/fill.py
+++ main/management/commands/fill.py
@@ -4,18 +4,35 @@ from main.models import Product, Category
 
 
 class Command(BaseCommand):
+
     Product.objects.all().delete()
     Category.objects.all().delete()
 
     def handle(self, *args, **options):
-        product_list = [
-            {'category': 'Фрукты', 'description': 'Сладкие и вкусные'},
-            {'category': 'Овощи', 'description': 'Большой выбор разных'},
-            {'category': 'Орешки', 'description': 'Калорийные и хрустящие'},
+        product_list = [{"name": "Яблоко",
+             "category": "Фрукты",
+             "price": 10,
+             },
+            {"name": "Фундук",
+             "category": "Орехи",
+             "price": 100,
+             },
+            {"name": "Огурец",
+             "category": "Овощи",
+             "price": 15,
+             },]
+        category_list = [
+            {'category': 'Фрукты'},
+            {'category': 'Овощи'},
+            {'category': 'Орешки'},
         ]
 
         category_for_create = []
+        product_for_create = []
         for product_item in product_list:
-           category_for_create.append(Category(**product_item))
+           product_for_create.append(Product(**product_item))
+
+        for category_item in category_list:
+            category_for_create.append(Category(**category_item))
 
         Category.objects.bulk_create(category_for_create)
