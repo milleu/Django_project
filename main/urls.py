a595176a33e59e35449881e674b11ca7ed75147e
Daria Beliaeva <dariabeliaeva22@gmail.com>
2024-03-22 12:33:50 -0700

    static created

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from django.conf import settings
 from main.apps import AppConfig
 from main.views import home, contacts
+from django.conf.urls.static import static
 
 app_name = AppConfig.name
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
